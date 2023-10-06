bbf069cab96e149a12009a1c573558f64b198516
akotorri <artjola.kotorri@ikubinfo.al>
2023-10-06 12:34:17 +0200

    first commit

---
+++ src/main/java/com/ikubinfo/plumbershop/PlumberShopApplication.java
@@ -0,0 +1,13 @@
+package com.ikubinfo.plumbershop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlumberShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlumberShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ikubinfo/plumbershop/PlumberShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ikubinfo.plumbershop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlumberShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

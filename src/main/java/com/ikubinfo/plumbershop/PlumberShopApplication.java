0fa6f91ebb845daf572f6ce5b091535ab1585cca
Artjola Kotorri <artjolakotorri@gmail.com>
2023-10-14 23:17:28 +0200

    enable auditing

--- src/main/java/com/ikubinfo/plumbershop/PlumberShopApplication.java
+++ src/main/java/com/ikubinfo/plumbershop/PlumberShopApplication.java
@@ -2,8 +2,10 @@ package com.ikubinfo.plumbershop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
 
 @SpringBootApplication
+@EnableMongoAuditing
 public class PlumberShopApplication {
 
 	public static void main(String[] args) {

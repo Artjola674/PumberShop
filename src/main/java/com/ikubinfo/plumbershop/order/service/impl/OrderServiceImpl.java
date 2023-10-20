474d53bedebdaacebba80ee75f944c080717034e
akotorri <artjola.kotorri@ikubinfo.al>
2023-10-20 10:26:22 +0200

    subtract order amount from total when order is created

--- src/main/java/com/ikubinfo/plumbershop/order/service/impl/OrderServiceImpl.java
+++ src/main/java/com/ikubinfo/plumbershop/order/service/impl/OrderServiceImpl.java
@@ -6,11 +6,14 @@ import com.ikubinfo.plumbershop.order.mapper.OrderMapper;
 import com.ikubinfo.plumbershop.order.model.OrderDocument;
 import com.ikubinfo.plumbershop.order.repo.OrderRepository;
 import com.ikubinfo.plumbershop.order.service.OrderService;
+import com.ikubinfo.plumbershop.product.model.ProductDocument;
+import com.ikubinfo.plumbershop.product.repo.ProductRepository;
 import com.ikubinfo.plumbershop.security.CustomUserDetails;
 import com.ikubinfo.plumbershop.user.enums.Role;
 import com.ikubinfo.plumbershop.user.service.UserService;
 import org.mapstruct.factory.Mappers;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
 
@@ -20,13 +23,16 @@ public class OrderServiceImpl implements OrderService {
     private final OrderRepository orderRepository;
     private final OrderMapper orderMapper;
     private final UserService userService;
+    private final ProductRepository productRepository;
 
-    public OrderServiceImpl(OrderRepository orderRepository, UserService userService) {
+    public OrderServiceImpl(OrderRepository orderRepository, UserService userService, ProductRepository productRepository) {
         this.orderRepository = orderRepository;
         this.userService = userService;
+        this.productRepository = productRepository;
         this.orderMapper = Mappers.getMapper((OrderMapper.class));
     }
 
+    @Transactional
     @Override
     public OrderDto save(OrderDto orderDto, CustomUserDetails loggedUser) {
         OrderDocument orderDocument = orderMapper.toOrderDocument(orderDto);
@@ -44,18 +50,33 @@ public class OrderServiceImpl implements OrderService {
         double earnings = totalPrice - buyingPriceSum;
 
         orderDocument.setCustomer(userService.getUserByEmail(loggedUser.getEmail()));
-
         orderDocument.setTotalPrice(totalPrice);
         orderDocument.setEarnings(earnings);
         orderDocument.setDate(LocalDate.now());
+
+        subtractOrderAmountFromTotal(orderDocument);
+
+        //generateTheBill
+        //sendEmailToUser
 
         OrderDocument savedOrder = orderRepository.save(orderDocument);
 
         return orderMapper.toOrderDto(savedOrder);
     }
 
+    private void subtractOrderAmountFromTotal(OrderDocument orderDocument){
+        orderDocument.getOrderItems()
+                .forEach(orderItemDocument -> {
+                    ProductDocument productDocument = orderItemDocument.getProduct();
+                    productDocument.setCount(productDocument.getCount()-orderItemDocument.getAmount());
+                    productRepository.save(productDocument);
+                });
+    }
+
+
     private double calculateTotalProductsBuyingPrice(OrderDocument orderDocument) {
-        return orderDocument.getOrderItems().stream()
+        return orderDocument.getOrderItems()
+                .stream()
                 .mapToDouble(orderItemDocument ->
                         orderItemDocument.getAmount()
                                 * orderItemDocument.getProduct().getBuyingPrice())
